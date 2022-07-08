c47c32038468dc4452893b65c3c979cce817dc4b
Seungmin Lee <lsmin0703@telcoware.com>
2022-07-08 11:42:52 +0900

    https://www.acmicpc.net/problem/1600 문제 풀이 :light_bulb:

--- baekjoon/10. MONKEY(1600)/main.go
+++ baekjoon/10. MONKEY(1600)/main.go
@@ -1,6 +1,6 @@
 package main
 
-//https://www.acmicpc.net/problem/2178
+//https://www.acmicpc.net/problem/1600
 import (
 	"bufio"
 	"errors"
@@ -11,45 +11,59 @@ import (
 	"strings"
 )
 
+// 결과
+var ret int32 = -1
+
+// 입력값
 var k, h, w int32
+
+// 행렬 그래프
 var metrix [201][201]int32
-var cnt [201][201]int32
+
+// 방문 노드 확인 + 말의 이동 사용 여부
 var chk [201][201][31]bool
+
+// 원숭이 이동
 var mx = []int32{1, 0, -1, 0}
 var my = []int32{0, -1, 0, 1}
+
+// 말 이동
 var hx = []int32{2, 2, 1, 1, -2, -2, -1, -1}
 var hy = []int32{1, -1, 2, -2, 1, -1, 2, -2}
 
+// 좌표
 type Location struct {
-	X int32
-	Y int32
+	X int32 // x 좌표
+	Y int32 // y 좌표
+	A int32 // x,y 좌표까지 이동 시 말의 능력을 몇번 사용했는지 확인
+	M int32 // 현재까지 지나온 거리
 }
 
 func bfs(y, x int32) {
 	var q Que
 	var iy, ix int32
 
-	chk[y][x] = true
-	cnt[y][x] = 1
+	chk[y][x][0] = true
+
 	// Que Append
-	q.Enque(Location{X: x, Y: y})
+	q.Enque(Location{X: x, Y: y, A: 0, M: 0})
 
 	for !q.IsEmpty() {
 		l, _ := q.Deque()
-		fmt.Printf("pod location %v\n", l)
-		if k > 0 {
+		if l.(Location).X == w-1 && l.(Location).Y == h-1 {
+			ret = l.(Location).M
+			break
+		}
+
+		if l.(Location).A < k {
 			for i := 0; i < 8; i++ {
 				iy = l.(Location).Y + hy[i]
 				ix = l.(Location).X + hx[i]
 				if (0 <= ix && ix < w) && (0 <= iy && iy < h) {
-					if metrix[iy][ix] == 0 && !chk[iy][ix] {
-						cnt[iy][ix] = cnt[l.(Location).Y][l.(Location).X] + 1
-						chk[iy][ix] = true
-						q.Enque(Location{X: ix, Y: iy})
+					if metrix[iy][ix] == 0 && !chk[iy][ix][l.(Location).A] {
+						chk[iy][ix][l.(Location).A] = true
+						q.Enque(Location{X: ix, Y: iy, A: l.(Location).A + 1, M: l.(Location).M + 1})
 
-						if ix == w && iy == h {
-							break
-						}
 					}
 				}
 			}
@@ -58,21 +72,15 @@ func bfs(y, x int32) {
 				iy = l.(Location).Y + my[i]
 				ix = l.(Location).X + mx[i]
 				if (0 <= ix && ix < w) && (0 <= iy && iy < h) {
-					if metrix[iy][ix] == 0 && !chk[iy][ix] {
-						cnt[iy][ix] = cnt[l.(Location).Y][l.(Location).X] + 1
-						chk[iy][ix] = true
-						q.Enque(Location{X: ix, Y: iy})
+					if metrix[iy][ix] == 0 && !chk[iy][ix][l.(Location).A] {
+						chk[iy][ix][l.(Location).A] = true
+						q.Enque(Location{X: ix, Y: iy, A: l.(Location).A, M: l.(Location).M + 1})
 
-						if ix == w && iy == h {
-							break
-						}
 					}
 				}
 			}
 		}
-
 	}
-
 }
 
 func main() {
@@ -103,11 +111,7 @@ func main() {
 
 	bfs(0, 0)
 
-	if cnt[h][w] == 0 {
-		fmt.Fprintf(writer, "-1")
-	} else {
-		fmt.Fprintf(writer, "%d", cnt[h][w])
-	}
+	fmt.Fprintf(writer, "%d\n", ret)
 
 	writer.Flush()
 
