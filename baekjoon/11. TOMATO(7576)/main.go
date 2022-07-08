44fe528dfc3323f817ba1c977a5fe69ddd5748f0
Seungmin Lee <lsmin0703@telcoware.com>
2022-07-08 16:55:29 +0900

    문제 풀이중

--- baekjoon/11. TOMATO(7576)/main.go
+++ baekjoon/11. TOMATO(7576)/main.go
@@ -12,42 +12,87 @@ import (
 )
 
 // 입력값
-var h, w int32
+// m 가로 길이 , n 세로 길이
+var m, n int32
+
+// 출력
+// 모두 익지 못하는 case의 경우 -1
+// 이미 모두 익은   case의 경우 1
+// 모두 익은 상태의 case의 경우 해당 일수
 
 // 행렬 그래프
-var metrix [201][201]int32
+var metrix [1001][1001]int32
 
 // 좌표
 type Location struct {
 	X int32 // x 좌표
 	Y int32 // y 좌표
+	M int32 // 최소 일수
+}
+
+var chk [1001][1001]bool
+
+func bfs(y, x int32) int32 {
+	var q Que
+	var pos Location
+	var my = []int32{-1, 0, 1, 0}
+	var mx = []int32{0, 1, 0, -1}
+
+	l := Location{X: x, Y: y, M: 0}
+	chk[y][x] = true
+	q.Enque(l)
+
+	for !q.IsEmpty() {
+		l, _ := q.Deque()
+		pos = l.(Location)
+		fmt.Printf("x:%d y:%d m:%d\n", pos.X, pos.Y, pos.M)
+		for i := 0; i < 4; i++ {
+			ix := pos.X + mx[i]
+			iy := pos.Y + my[i]
+			if (0 <= ix && ix < m) && (0 <= iy && iy < n) {
+				if metrix[iy][ix] == 0 && !chk[iy][ix] {
+					chk[iy][ix] = true
+					q.Enque(Location{X: ix, Y: iy, M: pos.M + 1})
+				}
+			}
+		}
+	}
+	return pos.M + 1
 }
 
 func main() {
 	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
 	writer := bufio.NewWriterSize(os.Stdout, 16*1024*1024)
 
-	whString := strings.Split(strings.TrimSpace(readLine(reader)), " ")
-	wTemp, err := strconv.ParseInt(whString[0], 10, 64)
+	tempString := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+	mTemp, err := strconv.ParseInt(tempString[0], 10, 64)
 	checkError(err)
-	w = int32(wTemp)
-	hTemp, err := strconv.ParseInt(whString[1], 10, 64)
+	m = int32(mTemp)
+	nTemp, err := strconv.ParseInt(tempString[1], 10, 64)
 	checkError(err)
-	h = int32(hTemp)
+	n = int32(nTemp)
 
-	for i := 0; i < int(h); i++ {
+	for i := 0; i < int(n); i++ {
 		str := strings.Split(strings.TrimSpace(readLine(reader)), " ")
-		for j := 0; j < int(w); j++ {
-			mTemp, _ := strconv.ParseInt(string(str[j]), 10, 64)
-			metrix[i][j] = int32(mTemp)
+		for j := 0; j < int(m); j++ {
+			tempValue, _ := strconv.ParseInt(string(str[j]), 10, 64)
+			metrix[i][j] = int32(tempValue)
 		}
 
 	}
 
-	fmt.Fprintf(writer, "%d\n")
+	for i := 0; i < int(n); i++ {
+		for j := 0; j < int(m); j++ {
+			if metrix[i][j] == 0 && !chk[i][j] {
+				move := bfs(int32(i), int32(j))
+				fmt.Printf("%d\n", move)
+			}
+		}
+	}
+
+	//fmt.Fprintf(writer, "%d\n", cnt)
 
 	writer.Flush()
-
 }
 
 func readLine(reader *bufio.Reader) string {
