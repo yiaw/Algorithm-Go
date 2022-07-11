8b688ba343a67a6cfd50774c58ed189ba89e90fa
unknown <lsmin0703@gmail.com>
2022-07-11 23:30:07 +0900

    시간초과 오류 1차 수정, L/R 계산방식 오류 검증

--- baekjoon/12. DSLR(9019)/main.go
+++ baekjoon/12. DSLR(9019)/main.go
@@ -11,83 +11,84 @@ import (
 )
 
 // 입력값
-// m 가로 길이 , n 세로 길이
+// t : test case, a : src, b : dst
 var t, a, b int
-var ab [][]int
+var ab [][2]int
+var chk [10000]bool
 
 // D 는 각 자리수 2배로 근데 9999 보다 큰 수일 경우에만 10000 나눈 나머지
 // S 는 N에서 1을 뺀 결과 nd이 0이면 9999
 // L 은 각 자리수를 왼편으로 회전
 // R 은 각 자리수를 오른편으로 회전
+type Value struct {
+	cur   int    // 현재 int 값
+	order string // 여기까지 온 DSLR 계산 방식
+}
 
-func bfs(src, dst int) []string {
+func bfs(src, dst int) string {
 	var q Que
-	var dslr []string
-	m := make(map[int]int)
-	q.Enque(src)
-	for !q.IsEmpty() {
-		input := q.Deque()
-		if input == dst {
-			break
-		}
+	q.Enque(Value{cur: src})
+	chk[src] = true
 
-		for _, f := range []func(int) (int, string){D, S, R, L} {
-			cal, str := f(input.(int))
-			if _, ok := m[cal]; !ok {
-				m[cal] = 0
-				q.Enque(cal)
-				dslr = append(dslr, str)
+	for !q.IsEmpty() {
+		o := q.Deque()
+		v := o.(Value)
+		if v.cur == dst {
+			return v.order
+		}
+		for _, f := range []func(int) (int, string){D, S, L, R} {
+			vv, str := f(v.cur)
+			fmt.Println(v.cur, vv, str)
+			if vv == dst {
+				return v.order + str
+			}
+			if !chk[vv] {
+				chk[vv] = true
+				q.Enque(Value{cur: vv, order: v.order + str})
 			}
 		}
+
 	}
-	return dslr
+	return ""
 }
 
 func main() {
-	var res [][]string
+	var ret []string
 	reader := bufio.NewReader(os.Stdin)
 
 	tempString := readLine(reader)
 	t, _ = strconv.Atoi(tempString)
 
 	for i := 0; i < t; i++ {
-		ab = make([][]int, t)
-		res = make([][]string, t)
-		for j := 0; j < 2; i++ {
-			ab[i] = make([]int, 2)
-		}
+		ab = make([][2]int, t)
 	}
 
 	for i := 0; i < t; i++ {
 		abTemp := strings.Split(readLine(reader), " ")
 		ab[i][0], _ = strconv.Atoi(abTemp[0])
 		ab[i][1], _ = strconv.Atoi(abTemp[1])
-		ret := bfs(ab[i][0], ab[i][1])
-		res[i] = append(res[i], ret...)
 	}
 
 	for i := 0; i < t; i++ {
-		for _, s := range res[i] {
-			fmt.Print(s)
-		}
-		fmt.Println()
+		ret = append(ret, bfs(ab[i][0], ab[i][1]))
 	}
+
+	for _, s := range ret {
+		fmt.Println(s)
+	}
+
 }
 
 func readLine(reader *bufio.Reader) string {
 	buff, _, err := reader.ReadLine()
-	if err != io.EOF {
+	if err == io.EOF {
 		return ""
 	}
 	return strings.TrimRight(string(buff), "\r\n")
 }
 
 func D(input int) (int, string) {
-	ret := input * 2
-	if ret >= 10000 {
-		ret = ret % 10000
-	}
-	return ret, "D"
+	return (input * 2) % 10000, "D"
 }
 
 func S(input int) (int, string) {
@@ -98,27 +99,23 @@ func S(input int) (int, string) {
 }
 
 func R(input int) (int, string) {
+	// 1234 > 4123
+	// 1234 % 10 = 4 * 1000 + 1234/10 = 123 4123
 
-	v := NumberToArray(input)
-	last := v[3]
-	for i := 2; i <= 0; i-- {
+	// 100 > 010 : 10
 
-		v[i+1] = v[i]
-	}
-	v[0] = last
-	return ArrayToNumber(v), "R"
+	return (input%10)*1000 + (input / 10), "R"
 }
 
 func L(input int) (int, string) {
-	v := NumberToArray(input)
-	last := v[0]
-	for i := 2; i <= 0; i-- {
-		v[i] = v[i+1]
-	}
-	v[3] = last
-	return ArrayToNumber(v), "L"
+	// 1234 > 2341
+	// 1234 % 1000 = 234*10 + 1234 / 1000 = 2340 + 1
+
+	// 100 > 001 : 1
+	return (input%1000)*10 + (input / 1000), "L"
 }
 
+// Que 자료 구조
 type Que []interface{}
 
 func (q *Que) IsEmpty() bool {
@@ -138,28 +135,4 @@ func (q *Que) Deque() interface{} {
 	*q = (*q)[1:]
 
 	return data
-}
-
-func NumberToArray(input int) [5]int {
-	data := input
-	var v [5]int
-	index := 3
-	for data != 0 {
-		if index < 0 {
-			break
-		}
-		v[index] = data % 10
-		data /= 10
-		index--
-	}
-	return v
-}
-func ArrayToNumber(input [5]int) int {
-	var ret int
-	var std int = 1000
-	for i := 0; i < 4; i++ {
-		ret += input[i] * std
-		std /= 10
-	}
-	return ret
 }
